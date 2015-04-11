6b7094c9441c289ab11ca3d42dfd62bdb9d01dce
Cornelius Murphy <corneliusmurphy@corneliussmbp3.attlocal.net>
2015-04-11 14:10:16 -0500

    Now on to payments, video uploads, Event calendar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150409234540) do
+ActiveRecord::Schema.define(version: 20150410232235) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
