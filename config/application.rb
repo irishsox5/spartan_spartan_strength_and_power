5de830e45a7b6ad20d46bba52fe68e90ca237515
Cornelius Murphy <corneliusmurphy@CorneliussMBP3.attlocal.net>
2015-05-03 02:56:09 -0500

    More minor changes

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module SpartanStrengthAndPower
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.serve_static_assets = true
+    config.serve_static_files = true
   end
 end
