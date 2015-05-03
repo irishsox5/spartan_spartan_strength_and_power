f264d8c4755ebe169e4d54551ab8df86abff00b4
Cornelius Murphy <corneliusmurphy@CorneliussMBP3.attlocal.net>
2015-05-03 17:42:33 -0500

    devise mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,8 @@ Rails.application.configure do
   # config.action_controller.asset_host = 'http://assets.example.com'
   config.assets.raise_runtime_errors = true
   config.assets.compile = true
-
+  config.action_mailer.default_url_options = { :host => 'www.spartanstrengthandpower.com' }
+  Rails.application.routes.default_url_options[:host] = 'www.spartanstrengthandpower.com'
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
